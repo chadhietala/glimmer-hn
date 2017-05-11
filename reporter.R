b20d466e3a7c8ebc877d3447253f1ef2f227d8a7
Chad Hietala <chadhietala@gmail.com>
2017-05-11 16:02:52 -0700

    Fix reporter

--- reporter.R
+++ reporter.R
@@ -11,16 +11,14 @@ testNames = lapply(config$servers, function(item) return(c(item$name)))
 
 control = phases[phases$set == testNames[1] & phases$phase == 'render' & phases$type == 'cumulative',]$ms
 experiment = phases[phases$set == testNames[2] & phases$phase == 'render' & phases$type == 'cumulative',]$ms
-
 result = wilcox.test(experiment, control, conf.int=TRUE)
 
 samples$type = factor(samples$type, levels=c('compile','run','js','gc','duration'))
 
-
 # by default string factors are ordered alphabetically
 phases$phase = factor(phases$phase,
-    levels=c('load','render'),
-    labels=c('Load','Render'))
+    levels=c('load','render', 'paint'),
+    labels=c('Load','Render', 'Paint'))
 phases$set = factor(phases$set, levels=testNames)
 library('ggplot2')
 
@@ -38,7 +36,7 @@ dev.off()
 png(file=paste(folder, 'phases.png', sep = ''), width=1024, height=768)
 ggplot(aes(y = ms, x = phase, color = set), data = phases) +
   facet_grid(type ~ ., scales='free_y') +
-  geom_boxplot(outlier.size=0.5, outlier.shape=4)
+  geom_boxplot(outlier.size=0.5, outlier.shape=4) +
   labs(title = paste0(testNames[2]," against control ", testNames[1]), color = "Set",
-  x = paste0("estimated shift is ", result$estimate, ", confidence interval from ", result$conf.int[1], " to ", result$conf.int[2], " for ", attr(result$conf.int, "conf.level"),", p.value: ", result$p.value))
+   x = paste0("estimated shift is ", result$estimate, ", confidence interval from ", result$conf.int[1], " to ", result$conf.int[2], " for ", attr(result$conf.int, "conf.level"),", p.value: ", result$p.value))
 dev.off()
