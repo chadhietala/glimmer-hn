00b80769775c33631766933c4f2248567cf4d539
Chad Hietala <chadhietala@gmail.com>
2017-05-23 10:45:22 -0700

    Move mocks to server

---
+++ server/mocks/top.js
@@ -0,0 +1,419 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(app) {
+  const express = require('express');
+  let topRouter = express.Router();
+
+  topRouter.get('/', function(req, res) {
+    res.send({
+      data: [
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "by": "wiradikusuma",
+          "descendants": 6,
+          "id": 14071939,
+          "kids": [
+            14072799,
+            14075091,
+            14075058,
+            14074326,
+            14074291
+          ],
+          "score": 73,
+          "time": 1491740887,
+          "title": "PlatformIO, open source ecosystem for IoT",
+          "type": "story",
+          "url": "http:\/\/platformio.org\/"
+        },
+        {
+          "by": "rbanffy",
+          "descendants": 69,
+          "id": 14072166,
+          "kids": [
+            14072474,
+            14074419,
+            14072718,
+            14072254,
+            14074690,
+            14073116,
+            14073262,
+            14072714
+          ],
+          "score": 166,
+          "time": 1491744417,
+          "title": "GCC 7 Release Series \u2013 Changes, New Features, and Fixes",
+          "type": "story",
+          "url": "https:\/\/gcc.gnu.org\/gcc-7\/changes.html"
+        },
+        {
+          "by": "donnemartin",
+          "descendants": 75,
+          "id": 14072291,
+          "kids": [
+            14072605,
+            14073437,
+            14072636,
+            14074531,
+            14074132,
+            14073261,
+            14072685,
+            14072567,
+            14072665,
+            14074260,
+            14073518,
+            14072901,
+            14073034,
+            14073414
+          ],
+          "score": 132,
+          "time": 1491746287,
+          "title": "Unreasonable Ineffectiveness of Machine Learning in Computer Systems Research",
+          "type": "story",
+          "url": "https:\/\/www.sigarch.org\/the-unreasonable-ineffectiveness-of-machine-learning-in-computer-systems-research\/"
+        },
+        {
+          "by": "Tomte",
+          "descendants": 110,
+          "id": 14072626,
+          "kids": [
+            14073126,
+            14074883,
+            14072832,
+            14072969,
+            14075569,
+            14073600,
+            14072874,
+            14074931,
+            14074896,
+            14075084,
+            14073317,
+            14074392,
+            14072859,
+            14072930,
+            14073324,
+            14072890,
+            14074537,
+            14073401,
+            14073444,
+            14073256,
+            14074051,
+            14073012,
+            14073498,
+            14072827,
+            14072971,
+            14073097,
+            14073004,
+            14073086,
+            14072922,
+            14073403,
+            14072947,
+            14073303,
+            14073304,
+            14073149,
+            14072967
+          ],
+          "score": 263,
+          "time": 1491750501,
+          "title": "Textbook manifesto (2016)",
+          "type": "story",
+          "url": "http:\/\/greenteapress.com\/wp\/textbook-manifesto\/"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+            {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },    {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },{
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },{
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },{
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        },
+        {
+          "id": 14068253,
+          "by": "benbreen",
+          "descendants": 18,
+          "kids": [
+            14073095,
+            14075318,
+            14075158,
+            14073133
+          ],
+          "score": 123,
+          "time": 1491676205,
+          "title": "On the Women\u2019s Petition Against Coffee of 1674",
+          "type": "story",
+          "url": "https:\/\/resobscura.blogspot.com\/2017\/04\/that-newfangled-abominable-heathenish.html"
+        }
+      ],
+      meta: {
+        type: 'top',
+        from: 0,
+        to: 20,
+        max: 359,
+        pages: 18
+      }
+    });
+  });
+
+  topRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  topRouter.get('/:id', function(req, res) {
+    res.send({
+      'top': {
+        id: req.params.id
+      }
+    });
+  });
+
+  topRouter.put('/:id', function(req, res) {
+    res.send({
+      'top': {
+        id: req.params.id
+      }
+    });
+  });
+
+  topRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/top', require('body-parser').json());
+  app.use('/api/top', topRouter);
+};
