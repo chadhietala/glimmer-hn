9416b7c7b75b92aea7e592c85c0fa9bc73006fd8
Chad Hietala <chadhietala@gmail.com>
2017-05-29 15:43:58 -0700

    Nuke benchmarking stuff and update readme

--- benching/index.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-
-let HEAD = execSync(`git rev-parse HEAD`).toString()
-HEAD = HEAD.slice(0, HEAD - 1);
-
-execSync(`ember build --evironment=production`);
-execSync(`mv dist baseline-dist`);
-execSync(``);
